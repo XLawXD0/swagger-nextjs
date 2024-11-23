bbab787adaca1e37817622f020ca2608a3ebfd73
XLaw XD <101106351+XLawXD0@users.noreply.github.com>
2024-11-23 19:50:23 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,12 @@
 import '../style.css'; 
+import React from 'react';
+import App from 'next/app';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
+
+export default MyApp;
